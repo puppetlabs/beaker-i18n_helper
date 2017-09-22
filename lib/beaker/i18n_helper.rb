ff6b6f509a9ddebb206df317e626836ce310965a
Eric Putnam <putnam.eric@gmail.com>
2017-09-22 16:48:13 -0700

    changes method name, adds conditional logic
    changes name of install_language_pack_on method to install_language_on and adds conditional logic based on fact(operatingsystem). Updates docs accordingly.

--- lib/beaker/i18n_helper.rb
+++ lib/beaker/i18n_helper.rb
@@ -1,5 +1,6 @@
 require 'beaker'
 
+# Beaker i18n Helper
 module Beaker::I18nHelper # rubocop:disable Style/ClassAndModuleChildren
   include Beaker::DSL
   include Beaker::DSL::Helpers::FacterHelpers
@@ -38,14 +39,18 @@ module Beaker::I18nHelper # rubocop:disable Style/ClassAndModuleChildren
   # @param lang [String] the POSIX locale identifier as a string, e.g. 'ja_JP.utf8'
   # @return [nil]
   #
-  def install_language_pack_on(hsts, lang)
+  def install_language_on(hsts, lang)
     valid_lang_string?(lang)
     lang = parse_lang(lang)
 
     Array(hsts).each do |host|
       begin
-        install_package(host, 'systemd-services') unless shell('file /sbin/init').stdout =~ %r{systemd}
-        install_package(host, "language-pack-#{lang[0]}")
+        case fact('operatingsystem')
+        when 'Debian'
+          install_locales_all_on(host)
+        when 'Ubuntu'
+          install_language_pack_on(host, lang)
+        end
       rescue RuntimeError
         raise "Unable to install language pack for #{lang[0]} on #{host}"
       end
@@ -70,6 +75,28 @@ module Beaker::I18nHelper # rubocop:disable Style/ClassAndModuleChildren
         raise "Unable to change locale to #{lang} on #{host}"
       end
     end
+  end
+
+  private
+
+  # Installs the 'locales-all' package on the target host unless it is already installed
+  #
+  # @param host [Beaker::Host] beaker host
+  # @return [nil]
+  #
+  def install_locales_all_on(host)
+    install_package(host, 'locales-all') unless check_for_package(host, 'locales-all')
+  end
+
+  # Installs the language pack on the target host for a given language (if necessary)
+  #
+  # @param host [Beaker::Host] beaker host
+  # @param lang [String] the POSIX locale identifier as a string, e.g. 'ja_JP.utf8'
+  # @return [nil]
+  #
+  def install_language_pack_on(host, lang)
+    install_package(host, "language-pack-#{lang[0]}")
+    install_package(host, 'systemd-services') unless shell('file /sbin/init').stdout =~ %r{systemd}
   end
 end
 
